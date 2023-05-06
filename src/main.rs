2db3e4b021480fd09c9697b9dce41b779d09b88e
Amrendra Yadav <y.amren00@gmail.com>
2023-05-06 06:27:01 +0000

    First commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+/*
+Steps for this program
+1. take input in a mutable variable using io::stdin library
+2. generate a random number
+3. match the input variable with random number
+*/
+
+fn main() {
+    println!("Welcome to guessing game");
+    println!("Please input your guess");
+    //generate random number here
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop{
+        let mut guess = String::new();
+        io::stdin().
+            read_line(&mut guess).
+            expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter valid integer");
+                continue;
+            }
+        };
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Yay! You got it right");
+                break;
+            }
+        }
+    }
+}
